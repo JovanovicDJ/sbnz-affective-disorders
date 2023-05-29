5207463299aa5635afb44f1fa506b66932326f4a
GajicMladen <mladengajic00@gmail.com>
2023-05-29 14:57:23 +0200

    more tests

--- backend/service/src/test/java/com/ftn/sbnz/service/PsychopathyDisorderTest.java
+++ backend/service/src/test/java/com/ftn/sbnz/service/PsychopathyDisorderTest.java
@@ -14,7 +14,92 @@ import java.time.LocalDate;
 public class PsychopathyDisorderTest {
 
     @Test
-    public void LifstyleFactorTest(){
+    public void LifestyleFactorTest(){
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kc = ks.newKieClasspathContainer();
+        KieSession kieSession = kc.newKieSession("psychopathydisorder");
+
+        Patient patient = new Patient(0,"Mika","Mikic", LocalDate.of(2000, 2, 15),
+                GenderType.MALE,"mika@gmail.com","testpass", "0645533665");
+
+        kieSession.insert(patient);
+
+        Symptom symptom1 = new Symptom("need for stimulation", 5, SymptomGroup.LIFESTYLE_FACTOR, 0);
+        Symptom symptom2 = new Symptom("parasitic lifesytle", 5, SymptomGroup.LIFESTYLE_FACTOR, 0);
+        Symptom symptom3 = new Symptom("no long-term goals", 4, SymptomGroup.LIFESTYLE_FACTOR, 0);
+        Symptom symptom4 = new Symptom("no ambition", 3, SymptomGroup.LIFESTYLE_FACTOR, 0);
+        Symptom symptom5 = new Symptom("impulsiveness", 3, SymptomGroup.LIFESTYLE_FACTOR, 0);
+        Symptom symptom6 = new Symptom("irresponsibility", 3, SymptomGroup.LIFESTYLE_FACTOR, 0);
+
+
+        kieSession.insert(symptom1);
+        kieSession.insert(symptom2);
+        kieSession.insert(symptom3);
+        kieSession.insert(symptom4);
+        kieSession.insert(symptom5);
+        kieSession.insert(symptom6);
+
+
+        long ruleFireCount = kieSession.fireAllRules();
+        System.out.println(ruleFireCount);
+    }
+
+    @Test
+    public void AffectiveFactorTest(){
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kc = ks.newKieClasspathContainer();
+        KieSession kieSession = kc.newKieSession("psychopathydisorder");
+
+        Patient patient = new Patient(0,"Mika","Mikic", LocalDate.of(2000, 2, 15),
+                GenderType.MALE,"mika@gmail.com","testpass", "0645533665");
+
+        kieSession.insert(patient);
+
+        Symptom symptom1 = new Symptom("limited affect", 5, SymptomGroup.ANTISOCIAL_FACTOR, 0);
+        Symptom symptom2 = new Symptom("no empathy", 5, SymptomGroup.ANTISOCIAL_FACTOR, 0);
+        Symptom symptom3 = new Symptom("no responsibility", 4, SymptomGroup.ANTISOCIAL_FACTOR, 0);
+        Symptom symptom4 = new Symptom("no remorse", 3, SymptomGroup.ANTISOCIAL_FACTOR, 0);
+        Symptom symptom5 = new Symptom("no guilt", 3, SymptomGroup.ANTISOCIAL_FACTOR, 0);
+
+        kieSession.insert(symptom1);
+        kieSession.insert(symptom2);
+        kieSession.insert(symptom3);
+        kieSession.insert(symptom4);
+        kieSession.insert(symptom5);
+
+
+        long ruleFireCount = kieSession.fireAllRules();
+        System.out.println(ruleFireCount);
+    }
+
+    @Test
+    public void InterpersonalFactorTest(){
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kc = ks.newKieClasspathContainer();
+        KieSession kieSession = kc.newKieSession("psychopathydisorder");
+
+        Patient patient = new Patient(0,"Mika","Mikic", LocalDate.of(2000, 2, 15),
+                GenderType.MALE,"mika@gmail.com","testpass", "0645533665");
+
+        kieSession.insert(patient);
+
+        Symptom symptom1 = new Symptom("grandiosity", 5, SymptomGroup.ANTISOCIAL_FACTOR, 0);
+        Symptom symptom2 = new Symptom("pathological lying", 5, SymptomGroup.ANTISOCIAL_FACTOR, 0);
+        Symptom symptom3 = new Symptom("manipulativeness", 4, SymptomGroup.ANTISOCIAL_FACTOR, 0);
+        Symptom symptom4 = new Symptom("artificial charm", 3, SymptomGroup.ANTISOCIAL_FACTOR, 0);
+
+        kieSession.insert(symptom1);
+        kieSession.insert(symptom2);
+        kieSession.insert(symptom3);
+        kieSession.insert(symptom4);
+
+
+        long ruleFireCount = kieSession.fireAllRules();
+        System.out.println(ruleFireCount);
+    }
+
+    @Test
+    public void AntisocialFactorTest(){
         KieServices ks = KieServices.Factory.get();
         KieContainer kc = ks.newKieClasspathContainer();
         KieSession kieSession = kc.newKieSession("psychopathydisorder");
