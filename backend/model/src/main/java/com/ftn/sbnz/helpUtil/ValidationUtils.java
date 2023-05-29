010c58692bdb5badd083d78a0395c969cf00a9e5
Djordje Jovanovic <djordjejovanovic27@gmail.com>
2023-05-29 17:35:47 +0200

    Add more rules

--- backend/model/src/main/java/com/ftn/sbnz/helpUtil/ValidationUtils.java
+++ backend/model/src/main/java/com/ftn/sbnz/helpUtil/ValidationUtils.java
@@ -2,14 +2,13 @@ package com.ftn.sbnz.helpUtil;
 
 import com.ftn.sbnz.model.BipolarDisorder;
 import com.ftn.sbnz.model.DepressiveEpisode;
+import com.ftn.sbnz.model.ManicEpisode;
+import com.ftn.sbnz.model.RecurrentDepressiveDisorder;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.time.temporal.Temporal;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
 public class ValidationUtils {
 
@@ -60,9 +59,41 @@ public class ValidationUtils {
             Date nextExecutionDate = next.getExecutionTime();
             long monthsBetween = ChronoUnit.MONTHS.between((Temporal) currentExecutionDate, (Temporal) nextExecutionDate);
             if (monthsBetween > 2) {
-                return false; // Ako je razmak između dva susedna poremećaja veći od 2 meseca, ciklotimija nije prisutna
+                return false;
             }
         }
-        return true; // Ciklotimija je prisutna
+        return true;
+    }
+
+    public static boolean validateRecurrentDepressiveDisorder(List<DepressiveEpisode> bdsList) {
+        Collections.sort(bdsList, Comparator.comparing(DepressiveEpisode::getExecutionTime));
+        for (int i = 0; i < bdsList.size() - 1; i++) {
+            DepressiveEpisode current = bdsList.get(i);
+            DepressiveEpisode next = bdsList.get(i + 1);
+            Date currentExecutionDate = current.getExecutionTime();
+            Date nextExecutionDate = next.getExecutionTime();
+            long monthsBetween = ChronoUnit.MONTHS.between((Temporal) currentExecutionDate, (Temporal) nextExecutionDate);
+            if (monthsBetween > 3) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int calcIntensityRecurrentDisorder(List<DepressiveEpisode> bdsList) {
+        int intensitySum = 0;
+        for (DepressiveEpisode bds : bdsList) {
+            intensitySum += bds.getIntensitySum();
+        }
+        return intensitySum;
+    }
+
+    public static boolean validateDiagnosisChange(RecurrentDepressiveDisorder rdd, ManicEpisode me) {
+        Date rddExecutionTime = rdd.getExecutionTime();
+        Date meExecutionTime = me.getExecutionTime();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(rddExecutionTime);
+        calendar.add(Calendar.MONTH, 3);
+        return meExecutionTime.after(rddExecutionTime) && meExecutionTime.before(calendar.getTime());
     }
 }
